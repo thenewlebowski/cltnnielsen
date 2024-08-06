da811e2bf43a3b07ec682cea865a5e41ad0581b7
thenewlebowski <cltnnielsen@gmail.com>
2024-08-06 00:04:33 -0600

    remove console.log

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-  console.log(process.env)
 
   return defineConfig({
     plugins: [vue(), vueJsx()],
